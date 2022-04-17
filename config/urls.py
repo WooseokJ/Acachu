887d69b08043c17017d3b64cfa2410ecdc2e0027
Ganbilly <les1533@gmail.com>
2022-04-17 23:31:54 +0900

    ImageSearch 페이지 수정

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,5 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main),
-<<<<<<< Updated upstream
-=======
     path('', include('ImageSearchPage.urls'))
->>>>>>> Stashed changes
 ]
