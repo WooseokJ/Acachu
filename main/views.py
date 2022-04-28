a387b937749248af809ab7857279c0264c285eac
Hotsanit <useok056900@naver.com>
2022-04-28 12:59:24 +0900

    중간커밋

--- main/views.py
+++ main/views.py
@@ -19,7 +19,6 @@ def mypage(request):
 
 
 def login(request):
-    login_yn=False #로그인실패
     if request.method=='POST':
         user_account = request.POST.get('login_id', None)
         user_password = request.POST.get('login_pw', None)
@@ -27,11 +26,9 @@ def login(request):
             user = User.objects.get(user_account=user_account)    
             if PasswordHasher().verify(user.user_password, user_password):
                 request.session['user_id'] = user.user_id
-                login_yn=True #로그인성공
-                print(login_yn)
-                return render(request,'main/index.html',{'login_yn':login_yn})
+                return render(request,'main/index.html',{})
         except:
-            print(login_yn)
+            
             messages.warning(request,"로그인실패")
             return redirect("/")
     else:
@@ -48,7 +45,8 @@ def signup(request):
         account=request.POST.get('user_account',None)
         email=request.POST.get('User_email',None)
         password=request.POST.get('User_password',None)
-        re_password=request.POST.get('User_re_password',None)          
+        re_password=request.POST.get('User_re_password',None)  
+                
         if account !=None:
             if password !=re_password:
                 return render(request, 'main/index.html')
@@ -58,13 +56,19 @@ def signup(request):
                 noun = random.choice(Noun.objects.all())
                 nick = adj.first + ' ' + noun.second
                 password=PasswordHasher().hash(request.POST.get('User_password',None))
-                user=User.objects.create(user_account=account,
-                                    user_email=email,
-                                    user_password=password,
-                                    auth_id=1,
-                                    user_nickname=nick)
-                user.save()
-                return render(request,'main/index.html')
+                
+                try:
+                    user=User.objects.create(user_account=account,
+                                user_email=email,
+                                user_password=password,
+                                auth_id=1,
+                                user_nickname=nick)
+                    user.save()
+                    return render(request,'main/index.html')
+                except:
+                    messages.warning(request,'회원가입실패')
+                    return redirect('/')
+            
     else:
         return render(request,'main/index.html')
 
@@ -72,7 +76,6 @@ def signup(request):
 def mypage(request):
     user_id=request.session.get('user_id','0')
     if request.method=='POST': # 회원탈퇴
-        print('=========================')
         account=request.POST.get('User_account_mod',None)
         password_mod=PasswordHasher().hash(request.POST.get('User_password_mod',"0"))
         nickname=request.POST.get('User_nickname_mod',None)
@@ -93,9 +96,6 @@ def mypage(request):
         else: #회원삭제  
             password_del=request.POST.get('User_password_del',"0")
             user_info=User.objects.get(user_id=user_id) 
-            print(password_del)
-            print(user_info.user_password)
-            print(delete)
             try:
                 if PasswordHasher().verify(user_info.user_password, password_del):
                     if delete == '삭제':
@@ -110,15 +110,15 @@ def mypage(request):
         auth_id=user_info.auth_id
         if auth_id==1: #일반용
             auth_yn=False 
-            bookmark_info=Bookmark.objects.filter(user_id=user_info.user_id)
+            bookmark_info=Bookmark.objects.filter(user_id=user_info.user_id).order_by('-bookmark_reg_date')[:5]
             store_info=Store.objects.all()
-            review_info=Review.objects.filter(user_id=user_info.user_id)
+            review_info=Review.objects.filter(user_id=user_info.user_id).order_by('-review_mod_date')[:5]
             return render(request,'main/mypage.html',{'user_info':user_info,'bookmark_info':bookmark_info,
                                                       'review_info':review_info,'store_info':store_info,'auth_yn':auth_yn})
         else:           # 업주용
             auth_yn=True 
             print(auth_yn)
-            bookmark_info=Bookmark.objects.filter(user_id=user_info.user_id)
+            bookmark_info=Bookmark.objects.filter(user_id=user_info.user_id).order_by('-bookmark_reg_date')[:5]
             store_info=Store.objects.all()
             review_info=Review.objects.filter(user_id=user_info.user_id).order_by('-review_mod_date')[:5]
             return render(request,'main/mypage.html',{'user_info':user_info,'bookmark_info':bookmark_info,
