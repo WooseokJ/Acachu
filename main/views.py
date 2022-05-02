1bdb2169ad86b4e39f625222ceec7e1b519f7ad5
Hotsanit <useok056900@naver.com>
2022-05-02 13:27:56 +0900

    sidebar,color수정

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from asyncio.windows_events import NULL
 from audioop import reverse
+from pickle import NONE
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib import messages
@@ -19,24 +20,15 @@ def main(request):
             result.append(tmp)
         except:
             continue
-    print(result, 1)
-    main_yn=True
+    # print(result, 1)
+    
+    user_id=request.session.get('user_id','0')
     try:
-        user_id=request.session.get('user_id','0')
-        user_info=User.objects.get(user_id=user_id) 
-        auth_id=user_info.auth_id
-        if auth_id==2:
-            main_yn=False
-            return render(request,'main/index.html',
-                        {'main_yn':main_yn,
-                           'storetags':result})
-        return render(request,'main/index.html',
-                        {'main_yn':main_yn,'storetags':result})
-    except:
-        return render(request,'main/index.html',
-                        {'main_yn':main_yn,
-                         'storetags':result})
-
+        user_info = User.objects.get(user_id=user_id)    
+        request.session['auth_id'] = user_info.auth.auth_id
+        return render(request,'main/index.html',{'storetags':result,'user_info':user_info})
+    except:   
+        return render(request,'main/index.html',{'storetags':result})
 
 def mypage(request):
     return render(request,'main/mypage.html',{})
@@ -50,14 +42,8 @@ def login(request):
             user = User.objects.get(user_account=user_account)    
             if PasswordHasher().verify(user.user_password, user_password):
                 request.session['user_id'] = user.user_id
-                auth_id=user.auth_id
-                auth_yn2=True
-                if auth_id==2:
-                    auth_yn2=False
-                    return render(request,'main/index.html',{'auth_yn2':auth_yn2})
-                return render(request,'main/index.html',{'auth_yn2':auth_yn2})
+                return redirect('/')
         except:
-            
             messages.warning(request,"로그인실패")
             return redirect("/")
     else:
@@ -76,10 +62,16 @@ def signup(request):
         email=request.POST.get('User_email',None)
         password=request.POST.get('User_password',None)
         re_password=request.POST.get('User_re_password',None)  
+        
+        if User.objects.filter(user_account=account).exists():
+            messages.info(request,'이미있는 아이디입니다.')
+            
+        elif User.objects.filter(user_email=email).exists():
+            messages.info(request,'이미있는 이메일입니다.')
                 
-        if account !=None:
+        else:
             if password !=re_password:
-                return render(request, 'main/index.html')
+                return redirect('/')
 
             if password == re_password:
                 adj = random.choice(Adj.objects.all())
@@ -94,10 +86,11 @@ def signup(request):
                                 auth_id=1,
                                 user_nickname=nick)
                     user.save()
-                    return render(request,'main/index.html')
+                    return redirect('/')
                 except:
                     messages.warning(request,'회원가입실패')
                     return redirect('/')
+        return redirect('/')
             
     else:
         return render(request,'main/index.html')
